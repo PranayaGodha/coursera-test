41d56dec1ebfe30038628d9dd4f3bc37c7f7d2d9
Vijaya Sai Pranaya <vijayasai.godha@oracle.com>
2021-07-15 13:55:38 +0530

    Module 5 assignment

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -117,7 +117,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,
-                                                         "randomCategoryShortName",
+                                                         "{{randomCategoryShortName}}",
                                                          chosenCategoryShortName);
 
 
